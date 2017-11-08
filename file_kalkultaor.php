2aebbfaf47be35665d02bf4d479781616f86377c
ramdanjampang <33476513+ramdanjampang@users.noreply.github.com>
2017-11-08 14:48:20 +0700

    Update file_kalkultaor.php

--- file_kalkultaor.php
+++ file_kalkultaor.php
@@ -1,10 +1,3 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>pengembangan web dan ini adalah pengembangannya </title>
-</head>
-<body>
-<h1>plikasi kalkulator</h1>
-<h2>menggunakan github</h2>
-</body>
+
 </html>
